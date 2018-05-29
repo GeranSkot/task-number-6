3312ab6205497e663c0980d51a2feb9ad4543a8a
Aleksey <geranskot@gmail.com>
2018-05-29 15:53:22 +0300

    master

--- src/com/Aleksey/Main.java
+++ src/com/Aleksey/Main.java
@@ -5,37 +5,20 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        /*System.out.println("Сделаем проверку на создание треугольника");
-        System.out.println("Пожалуйста, введите сторону a: ");
-        Scanner A = new Scanner(System.in);
-	    int a = A.nextInt();
-        System.out.println("Пожалуйста, введите сторону b: ");
-	    Scanner B = new Scanner(System.in);
-	    int b = B.nextInt();
-        System.out.println("Пожалуйста, введите сторону c: ");
-	    Scanner C = new Scanner(System.in);
-	    int c = C.nextInt();
-    if ((a < b+c) && (b < a+c) && (c < a+b )) {
-        System.out.println("Треугольник существует");
-    }else{
-        System.out.println("Треугольник не существует");}*/
         triangle();
     }
 
     public static void triangle(){
         System.out.println("lets check this out for right triangle");
         System.out.print("enter pls side a: ");
-        Scanner A = new Scanner(System.in);
-        float a = A.nextFloat();
+        Scanner scan = new Scanner(System.in);
+        float a = scan.nextFloat();
         System.out.print("enter pls side b: ");
-        Scanner B = new Scanner(System.in);
-        float b = B.nextFloat();
+        float b = scan.nextFloat();
         System.out.print("one more, enter pls side c: ");
-        Scanner C = new Scanner(System.in);
-        float c = C.nextFloat();
-        //float d = c*c;
+        float c = scan.nextFloat();
         if ((c*c) == (a*a) + (b*b) || (a*a) == (c*c)+ (b*b) || (b*b) == (a*a) + (c*c) ) {
-            System.out.println("right triangle is exist, roooight");
+            System.out.println("right triangle is exist ");
         }else{
             System.out.println("mission failed, we gonna make next time");}
     }
